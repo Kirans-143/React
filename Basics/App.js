0e6e56c725d04c98821ee62e2cdac5f32e21b8c9
Kirans-143 <kiran.shet77@gmail.com>
2023-06-20 20:30:49 +0530

    Basics of React

---
+++ Basics/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from "./component/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello From React</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ Basics/component/Button.jsx
@@ -0,0 +1,10 @@
+const Button = () => {
+  return (
+    <div>
+      <a href="https://google.com" target="_blank" rel="noreferrer">
+        <button>Click Me</button>
+      </a>
+    </div>
+  );
+};
+export default Button;
